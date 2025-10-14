66f7b9380d5111091f45d9683e73c980b921d519
LuanRibeiroGit <luancoxa2009@gmail.com>
2025-10-14 15:30:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='text-green-200'>Hello World</h1>
+  )
+}
+
+export default App
