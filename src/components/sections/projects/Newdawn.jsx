c3db0a0939ca48891d2bbcc118281101d6112e5a
LuanRibeiroGit <luancoxa2009@gmail.com>
2025-11-01 12:14:51 -0300

    feat(fix): adicionando o video

--- src/components/sections/projects/Newdawn.jsx
+++ src/components/sections/projects/Newdawn.jsx
@@ -3,11 +3,13 @@ import { useLanguage } from '../../../contexts/LanguageContext'
 import { useTheme } from '../../../contexts/ThemeContext'
 
 
-import nd1 from './imgs/nd1.jpg'
-import nd2 from './imgs/nd2.jpg'
-import nd3 from './imgs/nd3.jpg'
+import nd1 from './imgs/nd1.png'
+import nd2 from './imgs/nd2.png'
+import nd3 from './imgs/nd3.png'
+import newdawn from './imgs/newdawn.mp4'
 
-const images = [
+const media = [
+    { type: "video", src: newdawn },
     nd1,
     nd2,
     nd3
@@ -17,7 +19,7 @@ export function Newdawn() {
     const { t } = useLanguage()
     const { theme } = useTheme()
 
-    const [selected, setSelected] = useState(images[0])
+    const [selected, setSelected] = useState(media[0])
 
     return (
         <div className='flex flex-col gap-5'>
@@ -26,8 +28,8 @@ export function Newdawn() {
                 <img src={selected} alt="" className='w-full sm:h-[400px] h-[150px] object-cover rounded-xl shadow-lg transition-all duration-300'/>
             </div>
             <div className='flex gap-2'>
-                {images.map((img, index) => (
-                    <button key={index} onClick={() => setSelected(images[index])}
+                {media.map((img, index) => (
+                    <button key={index} onClick={() => setSelected(media[index])}
                         className={`rounded-lg overflow-hidden`}
                     >
                         <img src={img} alt='' className="w-20 h-20 object-cover"
